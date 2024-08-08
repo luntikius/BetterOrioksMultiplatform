5ab0db5596248903039870595e4a37b576c07c83
luntikius <luntikius@gmail.com>
2024-08-08 23:49:22 +0300

    - test

--- composeApp/src/commonMain/kotlin/BetterOrioksApp.kt
+++ composeApp/src/commonMain/kotlin/BetterOrioksApp.kt
@@ -2,12 +2,10 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
-import androidx.navigation.compose.currentBackStackEntryAsState
 import androidx.navigation.compose.rememberNavController
 import model.AppScreens
 import ui.common.LoadingScreen
@@ -17,7 +15,7 @@ import ui.scheduleScreen.ScheduleScreen
 fun BetterOrioksApp(
     navController: NavHostController = rememberNavController()
 ) {
-    val backStackEntry by navController.currentBackStackEntryAsState()
+    // val backStackEntry by navController.currentBackStackEntryAsState()
 
     Scaffold(
         modifier = Modifier.fillMaxSize()
