61273d1b5b91a3581549e6811b25e5e50f7f12d1
luntikius <luntikius@gmail.com>
2024-08-09 12:54:59 +0300

    - updated BetterOrioksApp.kt

--- composeApp/src/commonMain/kotlin/BetterOrioksApp.kt
+++ composeApp/src/commonMain/kotlin/BetterOrioksApp.kt
@@ -2,10 +2,12 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
+import androidx.navigation.compose.currentBackStackEntryAsState
 import androidx.navigation.compose.rememberNavController
 import model.AppScreens
 import ui.common.LoadingScreen
