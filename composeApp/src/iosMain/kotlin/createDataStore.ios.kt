208a2caaac5e24042ce08ff4752318a79330f793
luntikius <luntikius@gmail.com>
2024-11-09 14:53:31 +0300

    BETTERORIOKS-10: fixed ios part

--- composeApp/src/iosMain/kotlin/createDataStore.ios.kt
+++ composeApp/src/iosMain/kotlin/createDataStore.ios.kt
@@ -1,24 +1,30 @@
 import androidx.datastore.core.DataStore
 import androidx.datastore.preferences.core.PreferenceDataStoreFactory
 import androidx.datastore.preferences.core.Preferences
+import kotlinx.cinterop.ExperimentalForeignApi
 import okio.Path.Companion.toPath
+import platform.Foundation.NSDocumentDirectory
+import platform.Foundation.NSFileManager
+import platform.Foundation.NSURL
+import platform.Foundation.NSUserDomainMask
 
 private fun createDataStore(producePath: () -> String): DataStore<Preferences> =
     PreferenceDataStoreFactory.createWithPath(
         produceFile = { producePath().toPath() }
     )
 
-internal const val dataStoreFileName = "dice.preferences_pb"
+internal const val DATA_STORE_FILE_NAME = "dice.preferences_pb"
 
+@OptIn(ExperimentalForeignApi::class)
 fun createDataStore(): DataStore<Preferences> = createDataStore(
     producePath = {
-        val documentDirectory: NSURL = NSFileManager.defaultManager.URLForDirectory(
+        val documentDirectory: NSURL? = NSFileManager.defaultManager.URLForDirectory(
             directory = NSDocumentDirectory,
             inDomain = NSUserDomainMask,
             appropriateForURL = null,
             create = false,
             error = null,
         )
-        requireNotNull(documentDirectory).path + "/$dataStoreFileName"
+        requireNotNull(documentDirectory).path + "/$DATA_STORE_FILE_NAME"
     }
 )
