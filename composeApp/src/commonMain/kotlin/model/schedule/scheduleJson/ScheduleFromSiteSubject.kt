c6709a2a6b19adfc56ca0ca2825200a7c2fde6a0
luntikius <luntikius@gmail.com>
2024-11-09 16:05:36 +0300

    BETTERORIOKS-10: added subjects loading logic

--- composeApp/src/commonMain/kotlin/model/schedule/scheduleJson/ScheduleFromSiteSubject.kt
+++ composeApp/src/commonMain/kotlin/model/schedule/scheduleJson/ScheduleFromSiteSubject.kt
@@ -3,7 +3,6 @@ package model.schedule.scheduleJson
 import kotlinx.serialization.SerialName
 
 @kotlinx.serialization.Serializable
-
 class ScheduleFromSiteSubject(
     @SerialName("Code")
     val code: Int,
@@ -15,14 +14,18 @@ class ScheduleFromSiteSubject(
     val teacher: String,
     @SerialName("Form")
     val form: String
-){
+) {
     val formFromString =
-        if (name.contains("["))
-            name.slice(name.indexOf("[")+1 until name.indexOf("]"))
-        else "Н/С"
+        if (name.contains("[")) {
+            name.slice(name.indexOf("[") + 1 until name.indexOf("]"))
+        } else {
+            "Н/С"
+        }
 
     val nameFromString =
-        if (name.contains("["))
+        if (name.contains("[")) {
             name.slice(0 until name.indexOf("["))
-        else name
+        } else {
+            name
+        }
 }
