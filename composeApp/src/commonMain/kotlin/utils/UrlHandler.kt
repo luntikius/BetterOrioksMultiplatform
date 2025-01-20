67c9baf3a50f31bdf58d0e1da02e54b781a23148
luntikius <luntikius@gmail.com>
2025-01-20 20:13:53 +0300

    BETTERORIOKS-12: added openMoodle to UrlHandler

--- composeApp/src/commonMain/kotlin/utils/UrlHandler.kt
+++ composeApp/src/commonMain/kotlin/utils/UrlHandler.kt
@@ -2,4 +2,7 @@ package utils
 
 interface UrlHandler {
     fun handleUrl(url: String)
+
+    fun openMoodle(scienceId: String) =
+        handleUrl("https://orioks.miet.ru/mdl-gateway/course?science_id=$scienceId")
 }
