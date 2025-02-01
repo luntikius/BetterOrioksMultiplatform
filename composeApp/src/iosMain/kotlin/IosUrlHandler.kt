57604a687bc827537e77507bdbf8769d91b10614
luntikius <luntikius@gmail.com>
2025-02-01 19:15:44 +0300

    fixed resources

--- composeApp/src/iosMain/kotlin/IosUrlHandler.kt
+++ composeApp/src/iosMain/kotlin/IosUrlHandler.kt
@@ -6,7 +6,11 @@ class IosUrlHandler : UrlHandler {
     override fun handleUrl(url: String) {
         val nsUrl = NSURL.URLWithString(url)
         if (nsUrl != null) {
-            UIApplication.sharedApplication.openURL(nsUrl)
+            UIApplication.sharedApplication.openURL(
+                nsUrl,
+                options = emptyMap<Any?, Any>(),
+                completionHandler = null
+            )
         }
     }
 }
