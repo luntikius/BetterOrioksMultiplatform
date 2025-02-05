0a341aa471c4fdc8a9732a1a0b3ad5b60e46f7ff
luntikius <luntikius@gmail.com>
2025-02-06 01:27:53 +0300

    fixed schedule names

--- composeApp/src/commonMain/kotlin/model/schedule/ScheduleElement.kt
+++ composeApp/src/commonMain/kotlin/model/schedule/ScheduleElement.kt
@@ -1,6 +1,12 @@
 package model.schedule
 
+import androidx.compose.runtime.Composable
+import betterorioks.composeapp.generated.resources.Res
+import betterorioks.composeapp.generated.resources.gap_hours
+import betterorioks.composeapp.generated.resources.gap_hours_with_minutes
+import betterorioks.composeapp.generated.resources.gap_minutes
 import kotlinx.datetime.LocalTime
+import org.jetbrains.compose.resources.pluralStringResource
 
 interface ScheduleElement {
     val day: Int
@@ -23,4 +29,19 @@ data class ScheduleGap(
     override val day: Int = 0,
     override val number: Int = 0,
     val gapDuration: Int = 0
-) : ScheduleElement
+) : ScheduleElement {
+
+    @Composable
+    fun getGapDurationString(): String {
+        val hours = gapDuration / 60
+        val minutes = gapDuration % 60
+        return if (hours == 0) {
+            pluralStringResource(Res.plurals.gap_minutes, minutes, minutes)
+        } else if (minutes == 0) {
+            pluralStringResource(Res.plurals.gap_hours, hours, hours)
+        } else {
+            pluralStringResource(Res.plurals.gap_hours_with_minutes, minutes, hours, minutes)
+        }
+    }
+
+}
