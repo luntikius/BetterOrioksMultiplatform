fb2b09243f1c3a71c37903c1862f90d8999d7999
luntikius <luntikius@gmail.com>
2025-02-07 20:44:15 +0300

    fixed language issues

--- composeApp/src/androidMain/kotlin/com/luntikius/betterorioks/MainActivity.kt
+++ composeApp/src/androidMain/kotlin/com/luntikius/betterorioks/MainActivity.kt
@@ -1,15 +1,29 @@
 package com.luntikius.betterorioks
 
 import App
+import android.content.res.Configuration
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import java.util.Locale
 
 class MainActivity : ComponentActivity() {
 
+    private fun setRussianLocale() {
+        val locale = Locale("ru")
+        Locale.setDefault(locale)
+
+        val config = Configuration(this.applicationContext.resources.configuration)
+        config.setLocale(locale)
+
+        this.applicationContext.createConfigurationContext(config)
+    }
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setRussianLocale()
         enableEdgeToEdge()
         setContent {
             App()
--- iosApp/iosApp/iOSApp.swift
+++ iosApp/iosApp/iOSApp.swift
@@ -3,7 +3,14 @@ import ComposeApp
 
 @main
 struct iOSApp: App {
+    
+    func setRussianLocale() {
+        UserDefaults.standard.set(["ru"], forKey: "AppleLanguages")
+        UserDefaults.standard.synchronize()
+    }
+    
     init() {
+        setRussianLocale()
         MainViewControllerKt.startKoin()
     }
     
