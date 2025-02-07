2818baeb2b14ac89bd58a0a7584a81b125893191
luntikius <luntikius@gmail.com>
2025-02-07 20:44:15 +0300

    fixed language issues

--- composeApp/src/commonMain/kotlin/App.kt
+++ composeApp/src/commonMain/kotlin/App.kt
@@ -1,12 +1,8 @@
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.text.intl.Locale
 import org.jetbrains.compose.ui.tooling.preview.Preview
 import org.koin.compose.KoinContext
 import org.koin.compose.viewmodel.koinViewModel
 import ui.theme.BetterOrioksTheme
-
-@Composable
-fun getLanguage() = Locale("ru")
 
 @Composable
 @Preview
