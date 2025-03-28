c076d32be02e54818568ebf1f9a6a797c48bcaa7
luntikius <luntikius@gmail.com>
2025-03-28 20:43:23 +0300

    fixed schedule bug

--- composeApp/src/commonMain/kotlin/utils/ScheduleUtils.kt
+++ composeApp/src/commonMain/kotlin/utils/ScheduleUtils.kt
@@ -16,25 +16,29 @@ object ScheduleUtils {
     )
 
     fun addGaps(elements: List<ScheduleClass>): List<ScheduleElement> {
-        return buildList {
-            for (j in 0..elements.size - 2) {
-                val left = elements[j]
-                val right = elements[j + 1]
-                val timeBetweenPairs: Duration =
-                    (right.fromTime.toSecondOfDay() - left.toTime.toSecondOfDay())
-                        .toDuration(DurationUnit.SECONDS)
-                add(left)
-                if (timeBetweenPairs.inWholeMinutes > DEFAULT_GAP_DURATION) {
-                    add(
-                        ScheduleGap(
-                            day = right.day,
-                            number = left.number,
-                            gapDuration = timeBetweenPairs.inWholeMinutes.toInt()
+        return if (elements.size == 1) {
+            elements
+        } else {
+            buildList {
+                for (j in 0..elements.size - 2) {
+                    val left = elements[j]
+                    val right = elements[j + 1]
+                    val timeBetweenPairs: Duration =
+                        (right.fromTime.toSecondOfDay() - left.toTime.toSecondOfDay())
+                            .toDuration(DurationUnit.SECONDS)
+                    add(left)
+                    if (timeBetweenPairs.inWholeMinutes > DEFAULT_GAP_DURATION) {
+                        add(
+                            ScheduleGap(
+                                day = right.day,
+                                number = left.number,
+                                gapDuration = timeBetweenPairs.inWholeMinutes.toInt()
+                            )
                         )
-                    )
-                }
-                if (j == elements.size - 2) {
-                    add(right)
+                    }
+                    if (j == elements.size - 2) {
+                        add(right)
+                    }
                 }
             }
         }
