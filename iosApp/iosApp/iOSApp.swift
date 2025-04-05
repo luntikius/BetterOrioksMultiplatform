98cbae125f23d1ddaa797dcd5be7cb32ba1f14ab
luntikius <luntikius@gmail.com>
2025-04-05 17:31:41 +0300

    ios app + gradle fix

--- iosApp/iosApp/iOSApp.swift
+++ iosApp/iosApp/iOSApp.swift
@@ -9,22 +9,18 @@ struct iOSApp: App {
     private let backgroundWorker = IosBackgroundWorker()
 
     private func registerBackgroundTasks() {
-        
-        let newsTask = BGTaskScheduler.shared.register(
+        BGTaskScheduler.shared.register(
             forTaskWithIdentifier: "com.luntikius.betterorioks.NewsNotifications",
             using: nil
         ) { task in
             self.handleBackgroundTask(task: task as! BGProcessingTask, taskType: .newsnotifications)
         }
-        if (newsTask == true) { print("registered task \(BackgroundTaskType.newsnotifications)") }
-        
-        let scheduleTask = BGTaskScheduler.shared.register(
+        BGTaskScheduler.shared.register(
             forTaskWithIdentifier: "com.luntikius.betterorioks.SubjectNotifications",
             using: nil
         ) { task in
             self.handleBackgroundTask(task: task as! BGAppRefreshTask, taskType: .subjectnotifications)
         }
-        if (scheduleTask == true) { print("registered task \(BackgroundTaskType.subjectnotifications)") }
     }
 
     private func handleBackgroundTask(
@@ -32,9 +28,7 @@ struct iOSApp: App {
         taskType: BackgroundTaskType
     ) {
         print("Executing \(taskType) background task.")
-        
         backgroundWorker.executeTask(taskType: taskType)
-        
         task.setTaskCompleted(success: true)
     }
     
